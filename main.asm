42987621b1f54442ef086c5c6753eab0c8ff40e6
bhg0023 <benjamingoldberg@my.unt.edu>
2025-04-25 13:04:39 -0500

    Adding symbol table functionality

--- main.asm
+++ main.asm
@@ -7,9 +7,12 @@
 
 
 .data
+    operationMsg: .asciiz "Do you wish to allocate (1) or deallocate (2) memory? (Type 1 or 2): "
+    flagErr: .asciiz "Invalid entry!"
     variablePrompt: .asciiz "Enter variable name: "
     userInput: .space 22
     chunkArray: .space 64 # 64 bytes for chunk availability (0 is free, 1 is allocated)
+    symbolTable: .space 250 # 10 entries * 25 bytes per entry
     mallocFailMsg: .asciiz "Error: No free memory chunks available. \n"
     sizePrompt: .asciiz "Enter size (bytes to allocate): "
     newLine: .asciiz "\n"
@@ -17,7 +20,48 @@
 .text
 
 main: 
-     # Prompt user to enter variable name
+    la $t2, chunkArray  # load address of the chunkArray into $t2
+    li $t0, 0   # make sure $t0 is initilized at 0. Chunk Array Counter
+    li $t6, 0   # consecutive free chunks counter, initilize to zero
+    li $s1, 0
+
+init_chunk_array_loop:
+    bge $s1, 64, prompt # if counter has reached 64, go to malloc
+    add $a3, $t2, $s1   # store chunkArray[$t0] in $a3
+    sb $zero, 0($a3)    # change chunkArray[$t0] to 0
+    addi $s1, $s1, 1    # iterate the counter
+    j init_chunk_array_loop
+
+prompt:
+    li $v0, 4
+    la $a0, newLine
+    syscall
+    
+    # Prompt user if malloc or dealloc
+    li $v0, 4
+    la $a0, operationMsg
+    syscall
+
+    # read from user
+    li $v0, 5
+    syscall
+    move $s2, $v0
+
+    beq $s2, 1, malloc
+    beq $s2, 2, free
+
+    li $v0, 4
+    la $a0, flagErr
+    syscall
+
+    j prompt
+
+malloc: 
+    # Procedure handling memory allocation
+    # Read user input -> Check Symbol Table -> Find available chunks -> write chunks
+    # $t0 is where the chunk index is located
+
+    # Prompt user to enter variable name
     li $v0, 4
     la $a0, variablePrompt
     syscall
@@ -37,7 +81,7 @@ main:
     la $a0, sizePrompt
     syscall
 
-    # read integral from user
+    # read integerfrom user
     li $v0, 5
     syscall
     move $t1, $v0   # $s2 now holds bytes to allocate
@@ -46,11 +90,6 @@ main:
     move $s0, $sp
     li $t0, 0   # this will be the chunk index as it is allocated
     li $t4, 64 # total number of chunks to allocate
-
-malloc: 
-    # Procedure handling memory allocation
-    # Read user input -> Check Symbol Table -> Find available chunks -> write chunks
-    # $t0 is where the chunk index is located
     
     # calculate number of chunks to allocate
     li   $t8, 32         # chunk size
@@ -59,22 +98,11 @@ malloc:
     divu $t3, $t8        # unsigned division (avoid negatives)
     mflo $t9             # get number of chunks
 
-    la $t2, chunkArray  # load address of the chunkArray into $t2
-    li $t0, 0   # make sure $t0 is initilized at 0. Chunk Array Counter
-    li $t6, 0   # consecutive free chunks counter, initilize to zero
-    li $s1, 0
-
-init_chunk_array_loop:
-    bge $s1, 64, malloc_loop # if counter has reached 64, go to malloc
-    add $a3, $t2, $s1   # store chunkArray[$t0] in $a3
-    sb $zero, 0($a3)    # change chunkArray[$t0] to 0
-    addi $s1, $s1, 1    # iterate the counter
-    j init_chunk_array_loop
-
 malloc_loop:
     bge $t0, $t4, malloc_fail # if loop gets through all chunks ($t0 >= 64), no free memory (jump to malloc_fail)
     add $a3, $t2, $t0
     lb $t3, 0($a3)    # load chunkArray[$t0] into $t3
+    li $t6, 0   # reset consecutive free chunk counter
     beq $t3, $zero, free_chunk  # if chunk is free (0) jump to free_chunk
     addi $t0, $t0, 1   # increment the chunk index  
     j malloc_loop   # repeat until free chunk found or no chunks found
@@ -89,6 +117,7 @@ free_chunk:
 save_start: 
     move $t7, $t0   # save the current chunk counter to $t7
     li $t6, 1       # increment consecutive chunk counter
+    beq $t6, $t9, allocate_chunk    # if we only need 1 chunk allocated, go to allocate_chunk
     addi $t0, $t0, 1    # increment counter
     j malloc_loop
 
@@ -117,10 +146,9 @@ malloc_finish:
     syscall
 
     li $t0, 0          # Start at index 0
-    j print_chunk_array
-    # exit program
-    # li $v0, 10
-    # syscall
+    la $s0, symbolTable # load address of symbolTable into $s0
+    li $s2, 0   # initilize symbolTable index to 0
+    j find_free_slot
 
 malloc_fail:
     li $v0, 4
@@ -132,13 +160,36 @@ malloc_fail:
     syscall
 
     li $t0, 0          # Start at index 0
-    j print_chunk_array
-    # exit program
-    # li $v0, 10
-    # syscall
+
+find_free_slot: 
+    li $s3, 25  # number of bytes in an entry
+    li $t8, 10  # number of entries
+    mul $s4, $s2, $s3   # offset = index * number of bytes in an entry
+    add $s5, $s0, $s4   # $s5 contains symbolTable[$s4]
+    lb $s6, 0($s5)  # load the validity bit into $s6
+    beqz $s6, write_entry   # if validity bit is zero, go to write_entry (found empty slot)
+    add $s2, $s2, 1 # iterate symbolTable index
+    blt $t8, $s2, find_free_slot
+
+    # no free slots found on symbol table
+    li $v0, 4
+    la $a0, mallocFailMsg
+    syscall
+    j prompt
+
+write_entry:
+    li $t9, 1
+    sb $t9, 0($s5)  # write the validity bit as zero
+    la $a0, userInput   # store address of userInput
+    li $t0, 0   # char index init at 0
+
+copy_name_loop:
+    lb $t1, 0($a0)  # store first bit of userInput in $t1
+    sb $t1, 1($s0)  #  
+
+
 
 # Print the chunk array after allocation
-
 print_chunk_array:
     bge $t0, 64, done # Exit when we've printed all 64 chunks
     add $a3, $t2, $t0 # Address of chunkArray[$t0]
@@ -150,9 +201,8 @@ print_chunk_array:
     j print_chunk_array
 
 done:
-    # exit program
-    li $v0, 10
-    syscall
+    j prompt
 free: 
+    j prompt
     # Procedure for handling memory deallocation
     # Read user input -> Check Symbol table -> Remove from symbol table
