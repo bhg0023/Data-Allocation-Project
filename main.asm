da1fd043f3264fda50b2936ce853fe9377fbea18
bhg0023 <benjamingoldberg@my.unt.edu>
2025-04-28 21:34:45 -0500

    Working allocation and deallocation

--- main.asm
+++ main.asm
@@ -14,6 +14,9 @@
     chunkArray: .space 64 # 64 bytes for chunk availability (0 is free, 1 is allocated)
     symbolTable: .space 250 # 10 entries * 25 bytes per entry
     mallocFailMsg: .asciiz "Error: No free memory chunks available. \n"
+    noUserSlots: .asciiz "Error: No used slots in symbol table \n"
+    noFreeSlots: .asciiz "Error: No free slots in symbol table \n"
+    nameUsed: .asciiz "Error: Variable name already in use \n"
     sizePrompt: .asciiz "Enter size (bytes to allocate): "
     newLine: .asciiz "\n"
 
@@ -81,13 +84,11 @@ malloc:
     la $a0, sizePrompt
     syscall
 
-    # read integerfrom user
+    # read integer from user
     li $v0, 5
     syscall
-    move $t1, $v0   # $s2 now holds bytes to allocate
+    move $t1, $v0   # $t1 now holds bytes to allocate
 
-    # save the original stack pointer in $s0
-    move $s0, $sp
     li $t0, 0   # this will be the chunk index as it is allocated
     li $t4, 64 # total number of chunks to allocate
     
@@ -102,8 +103,8 @@ malloc_loop:
     bge $t0, $t4, malloc_fail # if loop gets through all chunks ($t0 >= 64), no free memory (jump to malloc_fail)
     add $a3, $t2, $t0
     lb $t3, 0($a3)    # load chunkArray[$t0] into $t3
+    beqz $t3, free_chunk  # if chunk is free (0) jump to free_chunk
     li $t6, 0   # reset consecutive free chunk counter
-    beq $t3, $zero, free_chunk  # if chunk is free (0) jump to free_chunk
     addi $t0, $t0, 1   # increment the chunk index  
     j malloc_loop   # repeat until free chunk found or no chunks found
 
@@ -137,10 +138,7 @@ allocate_loop:
     j allocate_loop
 
 malloc_finish: 
-    move $a0, $t0
-    li $v0, 1
-    syscall
-
+    move $v1, $t0   # save end index
     la $a0, newLine
     li $v0, 4   
     syscall
@@ -169,40 +167,161 @@ find_free_slot:
     lb $s6, 0($s5)  # load the validity bit into $s6
     beqz $s6, write_entry   # if validity bit is zero, go to write_entry (found empty slot)
     add $s2, $s2, 1 # iterate symbolTable index
-    blt $t8, $s2, find_free_slot
+    blt $s2, $t8, find_free_slot
 
     # no free slots found on symbol table
     li $v0, 4
-    la $a0, mallocFailMsg
+    la $a0, noFreeSlots
     syscall
     j prompt
 
 write_entry:
     li $t9, 1
-    sb $t9, 0($s5)  # write the validity bit as zero
+    sb $t9, 0($s5)  # write the validity bit as 1
     la $a0, userInput   # store address of userInput
-    li $t0, 0   # char index init at 0
+    li $t3, 0   # char index init at 0
 
 copy_name_loop:
     lb $t1, 0($a0)  # store first bit of userInput in $t1
-    sb $t1, 1($s0)  #  
+    addi $t4, $t3, 1    # copy index with offset 1 (for validity bit)
+    add $t9, $s5, $t4  # symbol table index + copy index with offset
+    sb $t1, 0($t9)  # store first byte of userInput into symbol table index 
+    beqz $t1, store_chunks  # if null char found, move to storing chunks
+    addi $a0, $a0, 1    # iterate to next char
+    addi $t3, $t3, 1    # iterate copy Index
+    j copy_name_loop    
 
+store_chunks:
+    # store from_chunk (byte 23 in symbol table line)
+    addi $t8, $s5, 23
+    sb $t7, 0($t8)
 
+    li $v0, 1
+    move $a0, $t7
+    syscall
+
+    li $v0, 4
+    la $a0, newLine
+    syscall
+
+    # store to_chunk (byte 24 in symbol table line)
+    addi $t8, $s5, 24
+    addi $t4, $v1, -1 # to_chunk = last allocated indexf
+    sb $t4, 0($t8)
+
+    li $v0, 1
+    move $a0, $t4
+    syscall
+
+    li $v0, 4
+    la $a0, newLine
+    syscall
+
+    j print_chunk_array
 
 # Print the chunk array after allocation
 print_chunk_array:
-    bge $t0, 64, done # Exit when we've printed all 64 chunks
+    la $t2, chunkArray
+    li $t0, 0   # reinitilize counter
+
+print_chunk_array_loop:
+    bge $t0, 64, prompt # Exit when we've printed all 64 chunks
     add $a3, $t2, $t0 # Address of chunkArray[$t0]
     lb $a1, 0($a3)    # Load the value of the chunk
     li $v0, 1         # Prepare to print the chunk value
     move $a0, $a1     # Move the chunk value to $a0
     syscall           # Print the chunk value
     addi $t0, $t0, 1  # Move to the next chunk
-    j print_chunk_array
+    j print_chunk_array_loop
 
-done:
-    j prompt
+
 free: 
-    j prompt
     # Procedure for handling memory deallocation
     # Read user input -> Check Symbol table -> Remove from symbol table
+
+    # Prompt user to enter variable name
+    li $v0, 4
+    la $a0, variablePrompt
+    syscall
+
+    # Store variable name
+    li $v0, 8 # prepare to read string
+    la $a0, userInput # load userInput variable address onto register
+    li $a1, 22 # max characters = 22
+    syscall
+
+    li $v0, 4
+    la $a0, userInput
+    syscall
+
+    la $s0, symbolTable # load address of symbol table into $s0
+    li $s2, 0   # init symbol table index to 0
+
+find_used_slot: 
+    li $t7, 0   # init char index to 0
+    li $s3, 25  # number of bytes in an entry
+    li $t8, 10  # number of entries
+    li $s7, 1 # for checking validity bit 1
+    mul $s4, $s2, $s3   # offset = index * number of bytes in an entry
+    add $s5, $s0, $s4   # $s5 contains symbolTable[$s4]
+    lb $s6, 0($s5)  # load the validity bit into $s6
+    beq $s6, $s7, read_name_loop   # if validity bit is 1, go to read name loop
+    addi $s2, $s2, 1 # iterate symbolTable index
+    blt $s2, $t8, find_used_slot
+
+    # no full slots, go to fail message
+    li $v0, 4
+    la $a0, noUsedSlots
+    syscall
+    j prompt
+
+read_name_loop:
+    lb $t1, 0($a0)  # store first bit of userInput in $t1
+    addi $a3, $t7, 1    # read index with offset 1 (for validity bit)
+    add $t9, $s5, $a3  # symbol table index + copy index with offset
+    lb $t3, 0($t9)  # load char from symbol table into $t3
+    bne $t1, $t3, next_slot    # check if char in userInput is the same as char in symbol table. If not equal, go back to find_used_slot
+    beqz $t1, remove_entry  # if null char found, go to remove entry
+    addi $a0, $a0, 1    # iterate to next char
+    addi $t7, $t7, 1    # iterate copy Index
+    j read_name_loop  
+
+next_slot:
+    addi $s2, $s2, 1
+    blt $s2, $t8, find_used_slot
+    li $v0, 4
+    la $a0, noUsedSlots
+    syscall
+    j prompt
+
+
+
+remove_entry:
+    li $t9, 0
+    sb $t9, 0($s5)  # write the validity bit as zero
+    la $a0, userInput   # store address of userInput
+    li $t7, 0   # char index init at 0
+
+    # read from_chunk (byte 23 in symbol table line)
+    addi $t8, $s5, 23
+    lb $t4, 0($t8)  # store from_chunk into $t5
+
+    # read to_chunk (byte 24 in symbol table line)
+    addi $t8, $s5, 24
+    lb $t5, 0($t8) # store to_chunk into $t5
+
+    la $t2, chunkArray  # load address of chunk array into $t2
+
+    # free chunks in the chunk array
+    move $t0, $t4   # start at from_chunk
+
+free_chunks_loop:
+    bgt $t0, $t5, free_done # if all chunks have been freed exit the loop
+    add $a3, $t2, $t0   # address of curent chunk
+    sb $zero, 0($a3)    # change current chunk to 0
+    addi $t0, $t0, 1    # iterate chunk counter
+    j free_chunks_loop  
+
+free_done:
+    li $t0, 0 # reset counter for printing
+    j print_chunk_array
